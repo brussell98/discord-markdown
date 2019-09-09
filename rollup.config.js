f2167eeddfc48ef40f0a10e2bc3a60591854d5cb
Dot <dot@lumzapp.com>
2019-09-09 03:45:33 +0200

    Add rollup and create bundle

---
+++ rollup.config.js
@@ -0,0 +1,21 @@
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import replace from 'rollup-plugin-replace';
+import { terser } from 'rollup-plugin-terser';
+
+export default {
+  input: 'index.js',
+  output: {
+    format: 'iife',
+    file: 'dist/discord-markdown.min.js',
+    name: 'discordMarkdown'
+  },
+  plugins: [
+    resolve(),
+    commonjs(),
+    replace({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    }),
+    terser()
+  ]
+}
