06f70a5fe111288384727ac1decd6ff12f2eb285
SrGobi <alejandrosanjuangallegos@gmail.com>
2024-02-09 15:54:31 +0100

    Backport Rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,26 +1,22 @@
-import commonjs from '@rollup/plugin-commonjs';
-import replace from '@rollup/plugin-replace';
-import terser from '@rollup/plugin-terser';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import replace from 'rollup-plugin-replace';
+import { terser } from 'rollup-plugin-terser';
 
 export default {
 	input: 'index.js',
-	external: ['highlight.js', 'simple-markdown'],
 	output: {
 		format: 'iife',
 		file: 'dist/discord-markdown-fix.min.js',
 		name: 'discordMarkdown',
-		exports: 'named',
-		globals: {
-			'simple-markdown': 'markdown',
-			'highlight.js': 'hljs',
-		},
+		exports: 'named'
 	},
 	plugins: [
+		resolve(),
 		commonjs(),
 		replace({
-			preventAssignment: true,
-			'process.env.NODE_ENV': JSON.stringify('production'),
+			'process.env.NODE_ENV': JSON.stringify('production')
 		}),
-		terser(),
-	],
-};
+		terser()
+	]
+}
